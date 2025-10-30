4d8e14754f5428435df1b6b44351644ebb2781cb
Wladimir Burbano <Ing.SIGECA1@asohofrucol.com.co>
2025-10-30 00:14:03 -0500

    feat: add routing and components for authentication and dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./modules/auth/auth.component').then((m) => m.AuthComponent),
+    children: [],
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./modules/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
+  { path: '**', redirectTo: 'login' },
+];
