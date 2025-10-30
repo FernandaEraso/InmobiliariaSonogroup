e5d9545b039a9c891affdc7d8516e80123aaca90
Wladimir Burbano <Ing.SIGECA1@asohofrucol.com.co>
2025-10-30 00:55:31 -0500

    feat: add Bootstrap for styling, implement authentication form, and enhance component structure

--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-auth',
+  imports: [ButtonModule, CardModule, PasswordModule, ReactiveFormsModule],
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  //*@Inject
+  private _fb = inject(FormBuilder);
 
-  constructor() { }
+  //*Formulario
+  form: FormGroup;
+  loading = false;
 
-  ngOnInit() {
+  constructor() {
+    this.form = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
+  ngOnInit() {}
+
+  onSubmit(): void {
+    if (this.form.invalid) return;
+    this.loading = true;
+    const { email, password } = this.form.value;
+    console.log(email, password);
+  }
 }
